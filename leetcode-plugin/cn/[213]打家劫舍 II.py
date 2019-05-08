7526b40d8698c24a77d1750a8ff9b0a81cdec726
Johny.Zheng <shun.johny@gmail.com>
2019-05-08 18:12:19 +0800

    update commit

--- leetcode-plugin/cn/[213]打家劫舍 II.py
+++ leetcode-plugin/cn/[213]打家劫舍 II.py
@@ -28,36 +28,77 @@ class Solution(object):
         """
 
         if nums:
-            if len(nums) == 1:
-                print(max(nums))
+            nums_sort = sorted(nums)
+            nums_len = len(nums)
+            nums_max_len = nums_len // 2
+            logging.info("nums_len {}".format(nums_len))
+            logging.info("nums_max_len {}".format(nums_max_len))
+            logging.info("nums_sort {}".format(nums_sort))
+            max_index = - nums_max_len
+            logging.info("max_index {}".format(max_index ))
+            max_list = []
 
-            elif len(nums) == 2:
-                print(None)
-                exit(0)
-            else:
-                nums_r = nums.copy()
-                if nums[0] < nums[-1]:
-                    nums_r.pop(0)
+            for i in range(max_index, 0):
+                if i != -1:
+                    if nums[i] == nums_sort[i]:
+                        i -= 1
+                        logging.info("最大值索引 {}".format(i))
+                        max_list.append(nums_sort[i])
+                        logging.info("最大值list {}".format(max_list))
+
+            logging.info("最大值为：{}".format(max(nums)))
+            if max(nums) == nums[0] or max(nums) == nums[-1]:
                 if nums[0] > nums[-1]:
-                    nums_r.pop()
-
-                if len(nums_r):
-                    if len(nums_r) == 2:
-                        print(None)
-                        exit(0)
-                    rob_list = nums_r[::2]
-                    logging.info("rob_list: {}".format(rob_list))
-                    print(sum(rob_list))
-
-                logging.info("nums_r: {}".format(nums_r))
-            logging.info("nums: {}".format(nums))
-
+                    max_list.append(nums[0])
+                else:
+                    max_list.append(nums[-1])
+                logging.info(max_list)
+                rob_list = max_list
+                print(sum(rob_list))
         else:
             raise IndexError
+
+        # if nums:
+        #     if len(nums) == 1:
+        #         print(max(nums))
+        #
+        #     elif len(nums) == 2:
+        #         print(None)
+        #         exit(0)
+        #     else:
+        #         nums_r = nums.copy()
+        #         if nums[0] < nums[-1]:
+        #             nums_r.pop(0)
+        #
+        #             if len(nums_r):
+        #                 print(nums_r)
+        #                 if len(nums_r) == 2:
+        #                     print(None)
+        #                     exit(0)
+        #                 rob_list = nums_r[1::2]
+        #                 print("rob_list: {}".format(rob_list))
+        #                 print(sum(rob_list))
+        #         else:
+        #             nums_r.pop()
+        #
+        #             if len(nums_r):
+        #                 print(nums_r)
+        #                 if len(nums_r) == 2:
+        #                     print(None)
+        #                     exit(0)
+        #                 rob_list = nums_r[::2]
+        #                 print("rob_list: {}".format(rob_list))
+        #                 print(sum(rob_list))
+        #
+        #         logging.info("nums_r: {}".format(nums_r))
+        #     logging.info("nums: {}".format(nums))
+        #
+        # else:
+        #     raise IndexError
 
 
 if __name__ == '__main__':
 
-    nums = [1, 2, 3, 4]
+    nums = [2, 3, 2, 6, 7, 8]
     result = Solution.rob(nums)
 
